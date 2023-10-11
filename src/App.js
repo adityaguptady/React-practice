9c2e23dab1e10291af7c6fc6aa52f3500cb09054
Aditya Gupta <adityaguptady@gmail.com>
2023-10-11 15:48:20 +0530

    Added unmounting component code

--- src/App.js
+++ src/App.js
@@ -2,22 +2,59 @@
 import './App.css';
 import React from 'react';
 
+class InnerComponent extends React.Component
+{
+  render()
+  {
+    return <div>Inner component is here</div>
+  }
+
+  componentDidMount()
+  {
+    console.log("####Mounting InnerComponent####")
+  }
+
+  componentWillUnmount()
+  {
+    console.log("****Unmounting InnerComponent****")
+  }
+
+}
+
 class App extends React.Component
 {
-  state = {counter:0}
+  state = {counter:0, innerComponent:<InnerComponent/>}
   constructor(props)
   {
     super(props)
     console.log("I am inside Constructor")
   }
+
   static getDerivedStateFromProps()
   {
     console.log("I am inside getDerivedStateFromProps")
+    return true
+  }
+
+  shouldComponentUpdate()
+  {
+    console.log("Inside shouldComponentUpdate")
+    return true
   }
 
   incrementCounter = () => 
   {
+    //updating state on function call
     this.setState({counter:this.state.counter+1})
+    if(this.state.counter % 2 == 0)
+    {
+      this.setState({innerComponent:<InnerComponent/>})
+    }
+    else
+    {
+      this.setState({innerComponent:<div>Unmounted</div>})
+    }
+    
   }
 
   // Overridden render function
@@ -30,8 +67,25 @@ class App extends React.Component
           <button onClick={this.incrementCounter}>Click me</button>
           <br/>
           Counter:{this.state.counter}
+          <br/>
+          <div>{this.state.innerComponent}</div>
         </div>
     )
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState)
+  {
+    console.log("Inside getSnapshotBeforeUpdate")
+    console.log("Previous Prop:")
+    console.log(prevProps)
+    console.log("Previous State:")
+    console.log(prevState)
+    console.log("--------------------")
+    console.log("Updated Prop:")
+    console.log(this.props)
+    console.log("Update State:")
+    console.log(this.state)
+    return true
   }
 
   componentDidMount()
