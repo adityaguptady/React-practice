4aaaf83011de8ffcd4aa52b3faca418ee440eddb
Aditya Gupta <adityaguptady@gmail.com>
2023-10-18 15:56:29 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 // class InnerComponent extends React.Component
 // {
@@ -21,60 +21,57 @@ import React from 'react';
 
 // }
 
-class App extends React.Component
-{
-  state = {todo: []}
+// class App extends React.Component
+// {
+//   state = {todo: []}
 
-  constructor(props)
-  {
-    super(props)
-    this.handleClick = this.handleClick.bind(this)
-  }
+//   constructor(props)
+//   {
+//     super(props)
+//     this.handleClick = this.handleClick.bind(this)
+//   }
   
-  handleClick(event)
-  {
-    event.preventDefault()
-    console.log("handleClick is clicked")
-    let newTodo = document.getElementById("addTodo").value
-    // let newTodoObject = {
-    //   id: new Date().getTime(),
-    //   text: newTodo,
-    // }
-    console.log("Text readed: "+newTodo)
-    // console.log(newTodoObject)
-    // //console.log(this.state)
-    this.state.todo.push(newTodo)
-    console.log(this.state)
-    document.getElementById("addTodo").value = ""
-    //this.setState({todo:this.state.todo})
-    //alert("clicked")
-  }
+//   handleClick(event)
+//   {
+//     event.preventDefault()
+//     //console.log("handleClick is clicked")
+//     let newTodo = document.getElementById("addTodo").value
+//     let newTodoObject = {
+//       id: new Date().getTime(),
+//       text: newTodo,
+//     }
+//     //console.log("Text readed: "+newTodo)
+//     // console.log(newTodoObject)
+//     // //console.log(this.state)
+//     //this.setState({counter:this.state.counter+1})
+//     this.state.todo.push(newTodoObject)
+//     this.setState({todo:this.state.todo})
+//     console.log(this.state)
+//     document.getElementById("addTodo").value = ""
+//     this.state.todo.map((tempTodo) =>{
+//       return ("ID: "+tempTodo.id+", Text: "+tempTodo.text)
+//     })
+//     //alert("clicked")
+//   }
 
-  print()
-  {
-    console.log("Print")
-    console.log(this.state)
-  }
   
-  render()
-  {
-    this.print()
-    return <div>
-      <h1>ToDo list</h1>
-      <form onSubmit={this.handleClick}>
-        <input type="text" id='addTodo'/>
-        <button type='submit'>Add To-do</button>
-      </form>
-      {/* <div>
-        <button type='submit'>Click me</button>
-        {this.state.todo.map((tempTodo) =>
-          <div className='todo' key={tempTodo.id}>
-            <div className='todo-text'>{tempTodo.text}</div>
-          </div>)
-        }
-      </div> */}
-    </div>
-  }
+//   render()
+//   {
+//     return <div>
+//       <h1>ToDo list</h1>
+//       <form onSubmit={this.handleClick}>
+//         <input type="text" id='addTodo'/>
+//         <button type='submit'>Add To-do</button>
+//       </form>
+//       <div>
+//         {this.state.todo.map((tempTodo) =>
+//         {
+//           return <div>{tempTodo.text}</div>
+//         })
+//         }
+//       </div>
+//     </div>
+//   }
 
 
   // state = {
@@ -144,18 +141,66 @@ class App extends React.Component
   //   console.log("I am inside componentDidMount")
   // }
   
-}
-
-// function App(props) 
+//}
+// function App(props)
 // {
-//   console.log(props.color)
-//   console.log(props.size)
-//   console.log(props.clickEvent)
+//   let [counter, setCounter] = useState(0)
+//   //counter = 0
+//   // console.log(props.color)
+//   // console.log(props.size)
+//   // console.log(props.clickEvent)
 //   return (
 //     <div className="App">
-//       <button onClick={props.clickEvent}>Click me</button>
+//       <button onClick={() => setCounter(counter+1)}>Click me</button>
+//       <div>
+//         Counter is:{counter}
+//       </div>
 //     </div>
 //   );
 // }
+
+function handleClick(event)
+{
+  event.preventDefault()
+  //console.log("handleClick is clicked")
+  let newTodo = document.getElementById("addTodo").value
+  let newTodoObject = {
+    id: new Date().getTime(),
+    text: newTodo,
+  }
+  //console.log("Text readed: "+newTodo)
+  // console.log(newTodoObject)
+  // //console.log(this.state)
+  //this.setState({counter:this.state.counter+1})
+  this.state.todo.push(newTodoObject)
+  this.setState({todo:this.state.todo})
+  console.log(this.state)
+  document.getElementById("addTodo").value = ""
+  this.state.todo.map((tempTodo) =>{
+    return ("ID: "+tempTodo.id+", Text: "+tempTodo.text)
+  })
+  //alert("clicked")
+}
+
+function App(props)
+{
+  const [todos, steTodo] = useState([])
+  return (
+    <div>
+       <h1>ToDo list</h1>
+       <form onSubmit={this.handleClick}>
+         <input type="text" id='addTodo'/>
+         <button type='submit'>Add To-do</button>
+       </form>
+       <div>
+         {this.state.todo.map((tempTodo) =>
+        {
+          return <div>{tempTodo.text}</div>
+        })
+        }
+      </div>
+    </div>
+  );
+}
 
 export default App;
