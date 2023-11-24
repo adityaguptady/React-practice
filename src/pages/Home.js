114a9f2bc99ca97cb4f43789d0bc21099edc2b00
Aditya Gupta <adityaguptady@gmail.com>
2023-11-24 14:38:35 +0530

    useContext

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import {useState, useEffect, createContext, useContext } from 'react';
 import './Home.css'
+import Component2 from './Component2';
 
-const UserContext = createContext()
+export const UserContext = createContext()
 
 function Home()
 {
@@ -48,47 +49,10 @@ function Home()
             <button onClick={()=>updateData()}>Update Data</button>
             <UserContext.Provider value={user}>
                 <h1>{`Hello ${user}!`}</h1>
-                <Component2 user={user} />
+                <Component2/>
             </UserContext.Provider>
         </div>)
 }
 
-function Component2() {
-    return (
-      <>
-        <h1>Component 2</h1>
-        <Component3/>
-      </>
-    );
-  }
-  
-  function Component3() {
-    return (
-      <>
-        <h1>Component 3</h1>
-        <Component4 />
-      </>
-    );
-  }
-  function Component4() {
-    return (
-      <>
-        <h1>Component 4</h1>
-        <Component5 />
-      </>
-    );
-  }
-  
-  function Component5() 
-  {
-    const userRef = useContext(UserContext)
-    return (
-      <>
-        <h1>Component 5</h1>
-        <h2>{`Hello ${userRef} again!`}</h2>
-      </>
-    );
-  }
-  
 
 export default Home;
